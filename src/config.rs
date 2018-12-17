3cffd4b1e61a55d129033365058042c964e0eca9
lausek <root@lausek.eu>
2018-12-17 22:49:49 +0100

    brightness scroll

--- src/config.rs
+++ src/config.rs
@@ -31,10 +31,38 @@ pub fn widgets() -> Vec<Box<dyn Widget>>
                 })
         });
 
+    // TODO: this blog is way too slow
+    let brightness = Scroll::new()
+        .command(Action::ScrollUp, "~/.config/scripts/brightness-lostatus +5")
+        .command(
+            Action::ScrollDown,
+            "~/.config/scripts/brightness-lostatus -5",
+        )
+        .command(Action::Status, "~/.config/scripts/brightness-lostatus")
+        .formatter(|state| {
+            state
+                .cmd_status
+                .as_ref()
+                .map_or(Err("no status cmd"), |cmd| {
+                    match crate::shell(cmd.as_ref()) {
+                        Ok(output) => {
+                            let status = output.lines().next().unwrap().to_string();
+                            let icon = match f64::from_str(status.as_ref()) {
+                                Ok(volume) => get_percentage_char(volume, &chars::VOLUME),
+                                _ => panic!("could not interpret status command as number"),
+                            };
+                            Ok(format!("{} {}", icon, status))
+                        }
+                        Err(_) => Err("status command failed"),
+                    }
+                })
+        });
+
     let headset = Toggle::new().command("~/.config/scripts/headset-switch-toggle");
 
     vec![
         Box::new(Focus::new()),
+        //Box::new(brightness),
         Box::new(volume),
         Box::new(headset),
         Box::new(Battery::new()),
